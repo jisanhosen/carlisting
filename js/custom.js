cc5dbd67e5acfb935a82a366b229f4aab59facb9
jisanhosen <jisanhosen3@gmail.com>
2018-09-05 20:38:52 +0600

    carlisting psd to html

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+    
+    
+        $('.slider').slick({
+                dots: true,
+                infinite: true,
+                speed: 300,
+                prevArrow: '.prev',
+                nextArrow: '.next',
+                autoplay: true,
+                autopalyspeed: 1500,
+        
+        });
+        
+        
+});
